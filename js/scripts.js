3311626138a2261ca95160902abf87dd399be7f8
Hannah Melendy and Mike Lofgren <mike@>
2019-06-11 11:22:07 -0700

    add logic to join letters back together with - replacements

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
+
+//backend
+
+
+//frontend
+
 $(document).ready(function() {
   $("#jumbler").submit(function(event) {
     var wordsInput = $("#words").val();
-    console.log(wordsInput);
+    var letters = wordsInput.split('');
+
+    //put wordsInput into an array
+
+    for(var i = 0; i <= letters.length; i++) {
+      if(letters[i] === 'a' || letters[i] === 'e' ) {
+         letters[i] = '-';
+      }
+    }
+
+    var backResult = letters.join('');
+
+
+
+
+    console.log(backResult);
+
+    // console.log(wordsInput);
+
     event.preventDefault();
   });
 });
