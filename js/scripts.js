b7ebcf68244f34673d64ff49e2a086550b34754d
Hannah Melendy and Mike Lofgren <mike@>
2019-06-11 11:59:05 -0700

    add css styles and div to show game.

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,26 @@
 
 $(document).ready(function() {
   $("#jumbler").submit(function(event) {
+    $("#magic").hide();
     var wordsInput = $("#words").val();
+
 
     //put wordsInput into an array
     var letters = wordsInput.split('');
 
-
+    //This for loop iterates through the variable letters and uses logic to pick out the vowels and replace them with dashes
     for(var i = 0; i <= letters.length; i++) {
-      if(letters[i] === 'a' || letters[i] === 'e' ) {
+      if(letters[i] === 'a' || letters[i] === 'e' || letters[i] === 'i' || letters[i] === 'o' || letters[i] === 'u' ) {
          letters[i] = '-';
       }
     }
 
     //join char/letters from array back into string
     var backResult = letters.join('');
-
+    //This will show the id jumble
+    $("#jumble").show();
+    //This will display what the user submitted
+    $("#jumble").text(backResult);
 
 
 
